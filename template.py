0d87134d63e3f6630beeb926d4f75eac67978c1a
deepg16 <deepgondane16@gmail.com>
2022-04-24 23:37:24 +0530

    Stage 3 - Train and Evaluate

--- template.py
+++ template.py
@@ -5,7 +5,8 @@ dirs = [
     os.path.join("data", "processed"),
     "notebooks",
     "saved_models",
-    "src"
+    "src",
+    "report"
 ]
 
 for dir_ in dirs:
@@ -17,7 +18,9 @@ files = [
     "dvc.yaml",
     "params.yaml",
     ".gitignore",
-    os.path.join("src", "__init__.py")
+    os.path.join("src", "__init__.py"),
+    os.path.join("report", "scores.json"),
+    os.path.join("report", "params.json")
 ]
 
 for file_ in files:
